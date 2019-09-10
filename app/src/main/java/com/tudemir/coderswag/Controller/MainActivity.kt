37e0c51b2d8ad27e3e791055003acb830827e161
tudemir <tugbademir4@gmail.com>
2019-09-10 09:47:05 +0300

    Added image view to list categories heavy way

--- app/src/main/java/com/tudemir/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/tudemir/coderswag/Controller/MainActivity.kt
@@ -2,22 +2,20 @@ package com.tudemir.coderswag.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import com.tudemir.coderswag.Model.Category
 import com.tudemir.coderswag.R
 import com.tudemir.coderswag.Services.DataService
+import com.tudemir.coderswag.adapters.CategoryAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-                android.R.layout.simple_list_item_1,
+        adapter = CategoryAdapter(this,
                 DataService.categories)
 
         categoryListView.adapter = adapter
